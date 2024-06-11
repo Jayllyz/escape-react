1e3ffce3f11fd0b31de696841a93307d16b927a1
Simon <szhang0709@gmail.com>
2024-06-11 17:17:48 +0200

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'react';
-import viteLogo from '/vite.svg';
-import reactLogo from './assets/react.svg';
 import Header from './components/Header';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
