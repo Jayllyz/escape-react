5959ea12f7a0a10b93b5ce12a8c6096608f0af85
Metololo <93775692+Metololo@users.noreply.github.com>
2024-06-13 20:30:17 +0200

    feat: add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,19 @@ import Booking from './components/Booking';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Sessions from './components/Sessions';
+import Hero from './components/Hero';
 import ContactForm from './components/ui/ContactForm';
 
 function App() {
   return (
     <html lang="fr">
       <main>
-        <header>
-          <Header />
-        </header>
-        <div>
-          <h1 className="flex justify-center bg-slate-400 py-4 text-white text-3xl font-bold">Nos Sessions</h1>
-        </div>
+        <Header />
+        <Hero />
         <Sessions />
         <Booking />
-        <div className="m-8">
-          <ContactForm />
+        <div id='contact-section' className="m-8">
+          <ContactForm  />
         </div>
         <Footer />
       </main>
