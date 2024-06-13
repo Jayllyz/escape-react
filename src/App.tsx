22a37e6014db06cbf10c2cc21172071473fb0f72
Simon <szhang0709@gmail.com>
2024-06-13 08:01:49 +0200

    feat: sessions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,56 @@
 import Header from './components/Header';
+import SessionInfo from './components/ui/SessionInfo';
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <html lang="fr">
+      <main>
+        <header>
+          <Header />
+        </header>
+        <div>
+          <h1 className="flex justify-center bg-slate-400 py-4 text-white text-3xl font-bold">Nos Sessions</h1>
+        </div>
+        <SessionInfo
+          title={'Fuir Jason'}
+          description={`Bienvenue dans "Fuir Jason", un jeu d'évasion palpitant où votre survie est en jeu. Vous êtes piégés dans
+              une cabane isolée, poursuivis par le redoutable Jason. Vous avez 60 minutes pour résoudre les énigmes,
+              trouver des indices cachés et déverrouiller les portes. Chaque minute qui passe rapproche Jason.
+              Travaillez en équipe, restez calmes et utilisez votre intelligence pour vous échapper. Ne laissez pas la
+              peur vous envahir, ou Jason aura gagné. Bonne chance, et que le jeu commence !`}
+          image={'jason.avif'}
+          imageSide={'left'}
+          minPlayers={2}
+          maxPlayers={4}
+        />
+        <SessionInfo
+          title={'Le labyrinthe'}
+          description={`Bienvenue dans "Le labyrinthe", un jeu d'évasion stimulant qui vous plongera dans un monde de mystère et de
+             défis. Vous êtes piégés dans un labyrinthe complexe, rempli de pièges et d'énigmes à résoudre. Vous avez 60 minutes 
+             pour trouver votre chemin, découvrir des indices cachés et déverrouiller les portes. Travaillez en équipe, 
+             communiquez efficacement et utilisez votre logique pour vous échapper. Le temps presse, et chaque mauvaise 
+             décision pourrait vous coûter cher. Bonne chance, et que le jeu commence !`}
+          image={'labyrinth.jpg'}
+          imageSide={'right'}
+          minPlayers={4}
+          maxPlayers={8}
+        />
+        <SessionInfo
+          title={"L'asile"}
+          description={`Plongés dans l'obscurité oppressante de l'asile abandonné de Ravenscroft, vous et votre équipe devez 
+            affronter vos peurs les plus profondes. Jadis un lieu de soins, l'asile est désormais le théâtre de phénomènes étranges 
+            et inexpliqués. Des cris résonnent dans les couloirs déserts, des ombres inquiétantes glissent sur les murs décrépis. 
+            Le personnel a disparu sans laisser de trace, et les patients errent, hantés par des esprits vengeurs. Votre mission :
+             découvrir les sombres secrets de Ravenscroft et trouver la sortie avant que la folie ne vous engloutisse. 
+             Mais attention, le temps presse, et l'asile ne vous laissera pas partir sans lutter. Oserez-vous défier les 
+             ténèbres et survivre à l'horreur de l'asile?`}
+          image={'asylum.avif'}
+          imageSide={'left'}
+          minPlayers={3}
+          maxPlayers={6}
+        />
+      </main>
+    </html>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,8 +8,8 @@ function Header() {
   };
 
   return (
-    <nav className="flex items-center justify-center">
-      <ul className="flex list-none p-0 m-0">
+    <nav className="grid py-8 items-center justify-center bg-slate-500 text-white">
+      <ul className="flex gap-8 list-none p-0 m-0">
         <li className="mx-2 relative group">
           <a href="/" className="block">
             Accueil
@@ -22,22 +22,22 @@ function Header() {
         </li>
         <li className="mx-2 relative">
           <button onClick={toggleDropdown} className="block" type="button">
-            Session
+            Sessions
           </button>
           {isDropdownOpen && (
-            <ul className="absolute left-0 mt-2 w-40 bg-white shadow-md">
+            <ul className="absolute left-0 mt-2 w-40 bg-gray-500 shadow-md">
               <li className="border-b">
-                <a href="/session1" className="block px-4 py-2 hover:bg-gray-200">
+                <a href="/session1" className="block px-4 py-2 hover:bg-gray-200 hover:text-black">
                   Fuir Jason
                 </a>
               </li>
               <li className="border-b">
-                <a href="/session2" className="block px-4 py-2 hover:bg-gray-200">
+                <a href="/session2" className="block px-4 py-2 hover:bg-gray-200 hover:text-black">
                   Le labyrinthe
                 </a>
               </li>
               <li>
-                <a href="/session3" className="block px-4 py-2 hover:bg-gray-200">
+                <a href="/session3" className="block px-4 py-2 hover:bg-gray-200 hover:text-black">
                   L'asile
                 </a>
               </li>
