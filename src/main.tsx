4e607a4ff616ef3748df28b70bf76682459c31ba
Antony David <antonydavid945@gmail.com>
2024-06-13 19:10:08 +0200

    fix: lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App.tsx';
 import Legals from './pages/legals.tsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 const rootElement = document.getElementById('root');
