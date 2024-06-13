ae4cbd027367550bc76e410a18bff6acdfc9fa00
Metololo <93775692+Metololo@users.noreply.github.com>
2024-06-13 19:38:22 +0200

    feat: add border to contact form (#13)
    * feat: add border to contact form
    
    * feat: add booking form
    
    * fix: lint

---
+++ src/components/Booking.tsx
@@ -0,0 +1,11 @@
+import BookingForm from './ui/BookingForm';
+
+function Booking() {
+  return (
+    <section className="flex items-center justify-center w-full h-[80vh] bg-cover bg-[url('https://images.unsplash.com/photo-1494376877685-d3d2559d4f82?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+      <BookingForm />
+    </section>
+  );
+}
+
+export default Booking;
