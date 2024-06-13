3663670ecbdebea08748462f198041048dc528dd
Antony David <antonydavid945@gmail.com>
2024-06-13 20:53:23 +0200

    fix: header dropdown links + add company name (#15)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,6 +10,7 @@ export default function Header() {
   return (
     <header>
       <nav className="grid py-8 items-center justify-center bg-slate-500 text-white">
+        <h1 className="text-2xl font-bold text-white bg-slate-500 pl-8 pr-4 py-2 text-center">La Maison Horrifique</h1>
         <ul className="flex gap-8 list-none p-0 m-0">
           <li className="mx-2 relative group">
             <a href="/" className="block">
@@ -28,17 +29,17 @@ export default function Header() {
             {isDropdownOpen && (
               <ul className="absolute left-0 mt-2 w-40 bg-gray-500 shadow-md">
                 <li className="border-b">
-                  <a href="/session1" className="block px-4 py-2 hover:bg-gray-200 hover:text-black">
+                  <a href="#Fuir Jason" className="block px-4 py-2 hover:bg-gray-200 hover:text-black">
                     Fuir Jason
                   </a>
                 </li>
                 <li className="border-b">
-                  <a href="/session2" className="block px-4 py-2 hover:bg-gray-200 hover:text-black">
+                  <a href="#Le labyrinthe" className="block px-4 py-2 hover:bg-gray-200 hover:text-black">
                     Le labyrinthe
                   </a>
                 </li>
                 <li>
-                  <a href="/session3" className="block px-4 py-2 hover:bg-gray-200 hover:text-black">
+                  <a href="#L'asile" className="block px-4 py-2 hover:bg-gray-200 hover:text-black">
                     L'asile
                   </a>
                 </li>
