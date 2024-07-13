2d7da2005e8232a47d7469c1631b2ddb57fe94d9
Metololo <93775692+Metololo@users.noreply.github.com>
2024-07-13 22:14:26 +0200

    feat: finish login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import Booking from './components/Booking';
-import Footer from './components/Footer';
-import Header from './components/Header';
 import Hero from './components/Hero';
 import Sessions from './components/Sessions';
 import ContactForm from './components/ui/ContactForm';
@@ -9,14 +7,12 @@ function App(): JSX.Element {
   return (
     <html lang="fr">
       <main>
-        <Header />
         <Hero />
         <Sessions />
         <Booking />
         <div id="contact-section" className="m-8">
           <ContactForm />
         </div>
-        <Footer />
       </main>
     </html>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App.tsx';
 import Legals from './pages/legals.tsx';
 import './index.css';
+import Footer from './components/Footer.tsx';
+import Header from './components/Header.tsx';
+import EmployeeDashboard from './pages/EmployeeDashboard.tsx';
 import Login from './pages/Login.tsx';
 
 const rootElement = document.getElementById('root');
@@ -11,11 +14,14 @@ if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/login" element={<Login />} />
           <Route path="/mention-legales" element={<Legals />} />
+          <Route path="/employeeDashboard" element={<EmployeeDashboard />} />
         </Routes>
+        <Footer />
       </Router>
     </React.StrictMode>,
   );
